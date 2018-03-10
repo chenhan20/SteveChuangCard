7c4748f6717cd4804bb235b41179f8406a07af2f
steve <a0911558945@gmail.com>
2018-03-10 17:40:33 +0800

    in beanwork commit

---
+++ public/js/Stevecard.js
@@ -0,0 +1,18 @@
+$(".gotop").click(function(){
+    jQuery("html,body").animate({
+        scrollTop:0
+    },1000);
+});
+$(window).scroll(function() {
+    if ( $(this).scrollTop() > 300){
+        $('.gotop').fadeIn("fast");
+    } else {
+        $('.gotop').stop().fadeOut("fast");
+    }
+});
+$('.goright').click(function(){
+    alert('goright');
+});
+$('.goleft').click(function(){
+    alert('goleft');
+});
--- routes/SteveCard.js
+++ routes/SteveCard.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('SteveCard', { title: 'Express' });
@@ -8,6 +9,10 @@ router.get('/', function(req, res, next) {
 /* GET home page. */
 router.get('/page1', function(req, res, next) {
   res.render('./Steve_page1');
+});
+/* GET home page. */
+router.get('/Fintness', function(req, res, next) {
+  res.render('./Fintness');
 });
 
 
