831b82b28c91829f0f265049dd3f128e31920ad6
Stevechuang <a0911558945@gmail.com>
2018-05-21 15:43:14 +0800

    判別自己與收到訊息與使用者加入的區隔

--- public/js/Demo03.js
+++ public/js/Demo03.js
@@ -12,20 +12,18 @@ let socket = io.connect();
 let yourName;
 let onlineUser=[];
 
-socket.on('chat message', function (data) {
-    console.log('OnlineUser' + data);
-    appendMessage(data.username+":"+data.msg);
-
-    // $('.OnlineUser').append(data);
-});
-
-let appendMessage =(msg) =>{
-    $('.Msgcontainer').append($('<li>').text(msg));
-    $('.Msgcontainer').scrollTop($('.ChatRoom').height())
-    // var scrollHeight = $('.ChatRoom').prop("scrollHeight");
-    // $('.ChatRoom').animate({scrollTop:scrollHeight}, 400);
-
-    // $('.ChatRoom').scrollIntoView(400);
+let appendMessageUser =(msg) =>{
+    $('.Msgcontainer').append($('<li class="User">').text(msg));
+    $('.Msgcontainer').scrollTop($('.ChatRoom').height());
+};
+let appendMessageOther =(user,msg) =>{
+    $('.Msgcontainer').append($('<li class="OtherName">').text(user));
+    $('.Msgcontainer').append($('<li class="other">').text(msg));
+    $('.Msgcontainer').scrollTop($('.ChatRoom').height());
+};
+let appendMessageMe =(msg) =>{
+    $('.Msgcontainer').append($('<li class="Me">').text(msg));
+    $('.Msgcontainer').scrollTop($('.ChatRoom').height());
 };
 let UpdateOnlineUser =(nicknamesArray) => {
     $('.OnlineUserList li').remove();
@@ -33,6 +31,14 @@ let UpdateOnlineUser =(nicknamesArray) => {
         $('.OnlineUserList').append($('<li>').text(value));
     }
 };
+socket.on('chat message', function (data) {
+    if(yourName!=data.username){
+        appendMessageOther(data.username,data.msg);
+    }else{
+        appendMessageMe(data.msg);
+    }
+});
+
 
 //送出聊天訊息
 let sendMsg =()=>{
@@ -40,45 +46,73 @@ let sendMsg =()=>{
     if(Msg==""){
         alert('請輸入內容');
         return;
+    }else if(Msg.length>60){
+        alert('你打太多字了低能兒');
+        return;
     }
     socket.emit('chat message' ,Msg);
+    $('.msg_content').focus();
     $('.msg_content').val('');
 };
 
 socket.on('add user',function(data){
-    UpdateOnlineUser(data.nicknamesArray);
-    appendMessage(data.username+"已加入");
+    onlineUser=data.nicknamesArray;
+    UpdateOnlineUser(onlineUser);
+    appendMessageUser(data.username+"已加入");
+    $('.msg_content').focus();
 });
 
 socket.on('user left',function(data){
-    // data.nicknamesArray可收到onlineUser了
+    onlineUser=data.nicknamesArray;
     if(data.username!=undefined){
-        UpdateOnlineUser(data.nicknamesArray);
-        appendMessage(data.username+"已離開");
+        UpdateOnlineUser(onlineUser);
+        appendMessageUser(data.username+"已離開");
     }
   });
 socket.on('ready',function(data){
     // data.nicknamesArray可收到onlineUser了
-    UpdateOnlineUser(data.nicknamesArray);
+    onlineUser=data.nicknamesArray;
+    UpdateOnlineUser(onlineUser);
   });
 
 
 $( document ).ready(function() {
+    $('.nickBox').focus();
     socket.emit('ready');
     $('.ChatRoom').hide();
 });
 
+let chkName=(Name)=>{
+    if(Name==""){
+        alert('請輸入名稱');
+        $('.nickBox').focus();
+        return false;
+    }
+    if(Name.length>10){
+        alert('你名子太長了幹');
+        $('.nickBox').focus();
+        return false;
+    }
+    if(onlineUser.indexOf(Name)>-1){
+        alert('此名稱此聊天室已經有人使用');
+        $('.nickBox').focus();
+        return false;
+    }
+    return true;
+};
 
 $('.Btn_addroom').click(function() {
     let nickBox=$('.nickBox').val();
-    if(nickBox==""){
-        alert('請輸入名稱');
+    if(chkName(nickBox)){
+        yourName=nickBox;
+        socket.emit('add user' ,nickBox);
+    
+        $('.ChatRoom').show();
+        $('.UserAdd').hide();
+    }else{
         return;
     }
-    socket.emit('add user' ,nickBox);
 
-    $('.ChatRoom').show();
-    $('.UserAdd').hide();
 
 });
 $('.Btn_sendMsg').click(function() {
