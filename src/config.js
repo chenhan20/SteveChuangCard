a4fe0dc0b97404f622f615ef315ab3e8f5f5d9b2
Stevechuang <a0911558945@gmail.com>
2018-05-31 10:46:16 +0800

    commit by steve config檔案範例上傳

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    fb: {
+      account: '<YOUR  ACCOUNT>',
+      password: '<YOUR PASSWORD>'
+    },
+    mysql: {
+      database: '<YOUR DB DATABASE>',
+      username: '<YOUR DB USER NAME>',
+      password: '<YOUR DB PASSWORD>',
+      host: '<YOUR DB HOST>'
+    }
+  };
