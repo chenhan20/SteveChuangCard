70e5673c8a263981497441f1de9f63fc6bf0417e
Stevechuang <a0911558945@gmail.com>
2018-08-11 22:33:24 +0800

    add 獎金計算

--- js/Demo05.js
+++ js/Demo05.js
@@ -14,9 +14,18 @@ const tabledata = {row1,row2,row3,row4,row5,row6,row7,row8,row9,row10};
 const totaldata = {total};
 
 
+let SelectTotal = [];
+
 let melaApp = new Vue({
     el: '.melaApp',
-    data:{page:'Money',tabledata:tabledata,totaldata:totaldata,showBox:'Nopage'},
+    data:{
+        page:'Money',
+        tabledata:tabledata,
+        totaldata:totaldata,
+        showBox:'Nopage',
+        TotalMoney:'',
+        TotalMoneyNumber:0,
+        TotalClass:''},
     methods:{
         money:function(event){
             self=this;
@@ -30,18 +39,21 @@ let melaApp = new Vue({
                 scrollTop: $('.item').offset().top
             }, 500, function() {
                 // lockChange=true;
+                //控制月份顯示字詞位置動畫
                 $('.OneMonth').animate({
-                    left: '70%',top:'20%'}, {duration: 1000});
+                    left: '70%',top:'17%'}, {duration: 1000});
                 $('.TwoMonth').animate({
-                    left: '70%',top:'35%'}, {duration: 1000});
+                    left: '70%',top:'34%'}, {duration: 1000});
                 $('.ThreeMonth').animate({
-                    left: '70%',top:'50%'}, {duration: 1000});
+                    left: '70%',top:'51%'}, {duration: 1000});
                 $('.FourMonth').animate({
-                    left: '70%',top:'65%'}, {duration: 1000});
+                    left: '70%',top:'68%'}, {duration: 1000});
                 $('.FiveMonth').animate({
-                    left: '70%',top:'80%'}, {duration: 1000});
-});
- 
+                    left: '70%',top:'85%'}, {duration: 1000});
+                $('.fa-calendar-check').animate({
+                    left: '55%',top:'50%'}, {duration: 1000});
+                        
+            });
           },
         one:function(event){
             self=this;
@@ -75,6 +87,65 @@ let melaApp = new Vue({
         NextBox:function(event){
             self=this;
             self.showBox='P2';
+        },
+        boxSelect:function(event){
+            self=this;
+            let checked = event.toElement.checked;
+            let selectText = event.currentTarget.id;
+            if(checked){
+                SelectTotal.push(selectText);
+            }else{
+                SelectTotal.splice(SelectTotal.indexOf(selectText),1);
+            }
+            Calculation(self,SelectTotal);
         }
     }
 });
+
+
+let Calculation = (self,SelectTotal) =>{
+    self.TotalMoneyNumber = 0;
+
+    for(let value of SelectTotal){
+        if(SelectTotal.length==1 && value !='20consumer'){
+            self.TotalClass = '請先勾選消費者 否則無法達成目標';
+            self.TotalMoney = '';
+            return;
+        }
+        switch(value) {
+            case '20consumer':
+                self.TotalClass = '總監3';
+                self.TotalMoneyNumber += 46666;
+                break;
+            case 'Operators_1':
+                self.TotalClass = '總監5';
+                self.TotalMoneyNumber += 91434;
+                break;
+            case 'Operators_2':
+                self.TotalClass = '總監7';
+                self.TotalMoneyNumber += 166712;
+                break;
+            case 'Operators_3':
+                self.TotalClass = '總監9';
+                self.TotalMoneyNumber += 329994;
+                break;
+            case 'Operators_4':
+                self.TotalClass = '資深總監1';
+                self.TotalMoneyNumber += 550072;
+                break;
+                
+        }
+        
+    }
+
+    // self.TotalClass = '總監3';
+    // self.TotalMoneyNumber = 43333;
+    self.TotalMoney = MoneyFormat(self.TotalMoneyNumber);
+};
+
+//格式化金額
+let MoneyFormat = (Money) =>{
+    let Format;
+    Format = '$' + parseInt(Money).toLocaleString('en-US'); 
+    return Format;
+};
